c605650f4dde214d7a5abbba3b51218ac14f4c45
minhmon173 <minhmon173@gmail.com>
2023-10-04 09:45:55 +0700

    Removed folder from repository

--- catkin_ws/build/ball_chaser/CMakeFiles/_ball_chaser_generate_messages_check_deps_Service1.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/_ball_chaser_generate_messages_check_deps_Service1"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/_ball_chaser_generate_messages_check_deps_Service1.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
