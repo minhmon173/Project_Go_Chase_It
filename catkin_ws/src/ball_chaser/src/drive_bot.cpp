ecb4190bc8fad383e0cbe7a22ac6d1acd7f479fd
minhmon173 <minhmon173@gmail.com>
2023-10-04 10:22:42 +0700

    Rearrange code snippets

--- catkin_ws/src/ball_chaser/src/drive_bot.cpp
+++ catkin_ws/src/ball_chaser/src/drive_bot.cpp
@@ -21,9 +21,11 @@ bool handle_drive_request(ball_chaser::DriveToTarget::Request &req , ball_chaser
         //---
         //string msg_feedback
      
-    //publish: linear_x angular_z
-    motor_command.linear.x = req.linear_x;//0.5;
-    motor_command.angular.z = req.angular_z;//0.5;
+    //publish: linear_x angular_z 
+    // linear_x;//0.5;
+    motor_command.linear.x = req.linear_x;
+    // angular_z;//0.5;
+    motor_command.angular.z = req.angular_z;
     // Publish angles to drive the robot
     motor_command_publisher.publish(motor_command);
 
@@ -50,15 +52,15 @@ int main(int argc, char** argv)
     ros::ServiceServer driveToTargetService = n.advertiseService("/ball_chaser/command_robot", handle_drive_request);
 
     // TODO: Delete the loop, move the code to the inside of the callback function and make the necessary changes to publish the requested velocities instead of constant values
-//    while (ros::ok()) {
-//        // Create a motor_command object of type geometry_msgs::Twist
-//        geometry_msgs::Twist motor_command;
-//        // Set wheel velocities, forward [0.5, 0.0]
-//        motor_command.linear.x = 0.5;
-//        motor_command.angular.z = 0.0;
-//        // Publish angles to drive the robot
-//        motor_command_publisher.publish(motor_command);
-//    }
+    // while (ros::ok()) {
+    //     // Create a motor_command object of type geometry_msgs::Twist
+    //     geometry_msgs::Twist motor_command;
+    //     // Set wheel velocities, forward [0.5, 0.0]
+    //     motor_command.linear.x = 0.5;
+    //     motor_command.angular.z = 0.0;
+    //     // Publish angles to drive the robot
+    //     motor_command_publisher.publish(motor_command);
+    // }
 
     // TODO: Handle ROS communication events
     ROS_INFO("drive_bot service started spin ");
